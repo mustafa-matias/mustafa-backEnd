c278ed2186c60407088792093a6a450892d41324
mustafa-matias <mustafamatias@hotmail.com>
2023-11-11 01:45:02 -0300

    modificacion forgotPassword

--- public/js/forgotPassword.js
+++ public/js/forgotPassword.js
@@ -1,42 +1,45 @@
-const forgotPasswordForm = document.getElementById("forgotPassword");
+document.addEventListener("DOMContentLoaded", function () {
+  const forgotPasswordForm = document.getElementById("forgotPassword");
 
-forgotPasswordForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const email = forgotPasswordForm.querySelector('input[name="email"]').value;
+  forgotPasswordForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = forgotPasswordForm.querySelector('input[name="email"]').value;
 
-  fetch("/api/sessions/forgotPassword", {
-    method: "POST",
-    body: JSON.stringify({ email }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => {
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        throw new Error("Usuario no registrado");
-      }
+    fetch("/api/sessions/forgotPassword", {
+      method: "POST",
+      body: JSON.stringify({ email }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    .then((data) => {
-      Swal.fire({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 5000,
-        title: "Verifique su casilla de correo",
-        icon: "success",
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error("Usuario no registrado");
+        }
+      })
+      .then((data) => {
+        Swal.fire({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 5000,
+          title: "Verifique su casilla de correo",
+          icon: "success",
+        });
+        setTimeout(() => {
+          window.location.href =
+            "https://mustafa-backend-production.up.railway.app/sessions/login";
+        }, 1000);
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: "error",
+          position: "top-end",
+          title: error.message,
+          background: "#000",
+        });
       });
-      setTimeout(() => {
-        window.location.href = "https://mustafa-backend-production.up.railway.app/sessions/login";
-      }, 1000);
-    })
-    .catch((error) => {
-      Swal.fire({
-        icon: "error",
-        position: "top-end",
-        title: error.message,
-        background: "#000",
-      });
-    });
+  });
 });
