35067864d475003a5933208024dfef39748e02ec
mustafa-matias <mustafamatias@hotmail.com>
2023-11-11 14:29:09 -0300

    debug

--- public/js/forgotPassword.js
+++ public/js/forgotPassword.js
@@ -4,17 +4,13 @@ document.addEventListener("DOMContentLoaded", function () {
   forgotPasswordForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const email = forgotPasswordForm.querySelector('input[name="email"]').value;
-    const emailJson = JSON.stringify({ email })
+    const emailJson = JSON.stringify({ email });
 
     fetch("/api/sessions/forgotPassword", {
       method: "POST",
-      body: emailJson,
-      headers: {
-        "Content-Type": "application/json",
-      },
+      body: emailJson
     })
-    .then((response) => {
-        console.log(emailJson);
+      .then((response) => {
         if (response.status === 200) {
           return response.json();
         } else {
@@ -22,7 +18,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       })
       .then((data) => {
-        console.log(emailJson);
         Swal.fire({
           toast: true,
           position: "top-end",
@@ -32,13 +27,10 @@ document.addEventListener("DOMContentLoaded", function () {
           icon: "success",
         });
         setTimeout(() => {
-          window.location.href =
-          `https://mustafa-backend-production.up.railway.app/sessions/login`;
+          window.location.href = `https://mustafa-backend-production.up.railway.app/sessions/login`;
         }, 1000);
       })
       .catch((error) => {
-        console.log(emailJson);
-
         Swal.fire({
           icon: "error",
           position: "top-end",
