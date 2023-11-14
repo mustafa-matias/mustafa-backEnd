f7d281d59674a2c66131eab2827acd547de881a3
mustafa-matias <mustafamatias@hotmail.com>
2023-11-14 01:56:03 -0300

    modificacion users.js y main.handlebars

--- public/js/users.js
+++ public/js/users.js
@@ -1,86 +1,124 @@
 document.addEventListener("DOMContentLoaded", function () {
   const elimanarInactivosButtom = document.getElementById("eliminarInactivos");
 
-  elimanarInactivosButtom.addEventListener("click", function () {
-    fetch(`/api/users`, {
-      method: "DELETE",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.status === "success") {
-          Swal.fire("Se eliminaron solo los usuarios inactivos por 2 días", data.message, "success");
-        } else {
-          Swal.fire("Error", "Error al eliminar el usuario", "error");
-        }
+  if (elimanarInactivosButton) {
+    elimanarInactivosButtom.addEventListener("click", function () {
+      fetch(`/api/users`, {
+        method: "DELETE",
       })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  });
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.status === "success") {
+            Swal.fire(
+              "Se eliminaron solo los usuarios inactivos por 2 días",
+              data.message,
+              "success"
+            );
+          } else {
+            Swal.fire("Error", "Error al eliminar el usuario", "error");
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
+  }
 
   let deleteButtons = document.querySelectorAll(".btn-delete-user");
-
-  deleteButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
-      let userId = button.getAttribute("data-user-id");
-      Swal.fire({
-        title: "¿Estás seguro?",
-        text: "¡No podrás revertir esto!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#d33",
-        cancelButtonColor: "#3085d6",
-        confirmButtonText: "Sí, eliminarlo",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          fetch(`/api/users/${userId}`, {
-            method: "DELETE",
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              if (data.status === "success") {
-                Swal.fire("Eliminado", data.message, "success");
-              } else {
-                Swal.fire("Error", "Error al eliminar el usuario", "error");
-              }
+  if (deleteButtons) {
+    deleteButtons.forEach(function (button) {
+      button.addEventListener("click", function () {
+        let userId = button.getAttribute("data-user-id");
+        Swal.fire({
+          title: "¿Estás seguro?",
+          text: "¡No podrás revertir esto!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#d33",
+          cancelButtonColor: "#3085d6",
+          confirmButtonText: "Sí, eliminarlo",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            fetch(`/api/users/${userId}`, {
+              method: "DELETE",
             })
-            .catch((error) => {
-              console.error("Error:", error);
-            });
-        }
+              .then((response) => response.json())
+              .then((data) => {
+                if (data.status === "success") {
+                  Swal.fire("Eliminado", data.message, "success");
+                } else {
+                  Swal.fire("Error", "Error al eliminar el usuario", "error");
+                }
+              })
+              .catch((error) => {
+                console.error("Error:", error);
+              });
+          }
+        });
       });
     });
-  });
+  }
   let updateButtons = document.querySelectorAll(".btn-update-user");
-
-  updateButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
-      let userId = button.getAttribute("data-update-id");
-      Swal.fire({
-        title: "¿Estás seguro que queres cambiar de rol?",
-        showCancelButton: true,
-        confirmButtonColor: "#d33",
-        cancelButtonColor: "#3085d6",
-        confirmButtonText: "Sí",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          fetch(`/api/users/role/${userId}`, {
-            method: "PUT",
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              if (data.status === "success") {
-                Swal.fire("Rol cambiado", data.message, "success");
-              } else {
-                Swal.fire("Error", "Error al cambiar el rol", "error");
-              }
+  if (updateButtons) {
+    updateButtons.forEach(function (button) {
+      button.addEventListener("click", function () {
+        let userId = button.getAttribute("data-update-id");
+        Swal.fire({
+          title: "¿Estás seguro que queres cambiar de rol?",
+          showCancelButton: true,
+          confirmButtonColor: "#d33",
+          cancelButtonColor: "#3085d6",
+          confirmButtonText: "Sí",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            fetch(`/api/users/role/${userId}`, {
+              method: "PUT",
             })
-            .catch((error) => {
-              console.error("Error:", error);
-            });
-        }
+              .then((response) => response.json())
+              .then((data) => {
+                if (data.status === "success") {
+                  Swal.fire("Rol cambiado", data.message, "success");
+                } else {
+                  Swal.fire("Error", "Error al cambiar el rol", "error");
+                }
+              })
+              .catch((error) => {
+                console.error("Error:", error);
+              });
+          }
+        });
       });
     });
-  });
+  }
 
+  const buttonGetUsers = document.getElementById("buttonGetUsers");
+  if (buttonGetUsers) {
+    buttonGetUsers.addEventListener("click", (e) => {
+      fetch("/users/details", {
+        method: "GET",
+      })
+        .then((result) => {
+          if (result.status === 200) {
+            setTimeout(function () {
+              window.location.replace(
+                "https://mustafa-backend-production.up.railway.app/users/details"
+              );
+            }, 1000);
+          } else {
+            Swal.fire({
+              toast: true,
+              position: "top-end",
+              showConfirmButton: false,
+              timer: 5000,
+              title: "Para ingresar debes ser Admin",
+              icon: "error",
+              background: "#000",
+            });
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
+  }
 });
